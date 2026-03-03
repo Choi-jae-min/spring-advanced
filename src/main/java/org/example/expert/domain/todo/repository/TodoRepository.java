c70c305cbaa7471c4913feff303e0c10f79f53d1
jasmin <cjm98@smartel.co.kr>
2026-03-03 13:02:31 +0900

    refactor :todo 조회시 n+1 문제를 fetch join에서  EntityGraph 로변경[Lv 3]
    Lv 3. N+1 문제
    - 기존 LEFT JOIN FETCH 를 EntityGraph 로변경
    EntityGraph 의 기본동작이 left join이지만 지금 실행 해보면 inner join을 수행 이는 hibernate가 user가 존재한다고 확신을 하고 inner조인을 수행한다.

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,12 +12,17 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    @EntityGraph(attributePaths = {"user"})
+    @Query("SELECT t FROM Todo t ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN FETCH t.user " +
-            "WHERE t.id = :todoId")
+
+    // Hibernate가 무조건 user가 존재한다고 확신할 수 있어서, EntityGraph 라도 INNER JOIN으로 최적화 될 수 있다.
+    @EntityGraph(attributePaths = {"user"})
+    @Query("""
+        SELECT t FROM Todo t
+        WHERE t.id = :todoId
+        """)
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
