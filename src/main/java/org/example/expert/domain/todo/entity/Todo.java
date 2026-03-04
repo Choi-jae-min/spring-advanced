44fc51f7e5cc224c36e6ba692c701bcd74ab9dfe
jasmin <cjm98@smartel.co.kr>
2026-03-04 10:26:54 +0900

    refactor : Todo comments,manager 불필요한 연관 관계 제거 [Lv 6]
    - 기존 서비스에서 todo 조회시 반환 데이터로 포함 되어있지 않으므로 삭제해도 서비스 흐름이 변하는게 없다고 판단

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -3,13 +3,9 @@ package org.example.expert.domain.todo.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.common.entity.Timestamped;
-import org.example.expert.domain.manager.entity.Manager;
 import org.example.expert.domain.user.entity.User;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Entity
@@ -27,18 +23,11 @@ public class Todo extends Timestamped {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
-    private List<Comment> comments = new ArrayList<>();
-
-    @OneToMany(mappedBy = "todo", cascade = CascadeType.PERSIST)
-    private List<Manager> managers = new ArrayList<>();
-
     public Todo(String title, String contents, String weather, User user) {
         this.title = title;
         this.contents = contents;
         this.weather = weather;
         this.user = user;
-        this.managers.add(new Manager(user, this));
     }
 
     public void update(String title, String contents) {
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -17,13 +17,10 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
 
-    // Hibernate가 무조건 user가 존재한다고 확신할 수 있어서, EntityGraph 라도 INNER JOIN으로 최적화 될 수 있다.
     @EntityGraph(attributePaths = {"user"})
     @Query("""
         SELECT t FROM Todo t
         WHERE t.id = :todoId
         """)
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
-
-    int countById(Long todoId);
 }
