6e6876a72c69ab9af959bc54e21a124445d927a8
jasmin <cjm98@smartel.co.kr>
2026-03-04 11:03:00 +0900

    refactor : AOP로깅 시스템 메서드 분리 [Lv 6]
    - HttpServletRequest 을 가져오는 부분 메서드 분리

--- src/main/java/org/example/expert/global/aop/LoggingAspect.java
+++ src/main/java/org/example/expert/global/aop/LoggingAspect.java
@@ -1,4 +1,4 @@
-package org.example.expert.config.aop;
+package org.example.expert.global.aop;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
@@ -30,20 +30,8 @@ public class LoggingAspect {
     @Around("adminApi()")
     public Object logAdminApi(ProceedingJoinPoint joinPoint) throws Throwable {
         UUID requestId = UUID.randomUUID();
-        // 요청 시간 확인
         String requestTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-
-        // attributes 가져오기
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-
-        //attributes 가 없으면 에러 발생
-        if(attributes == null){
-            log.error("HTTP 요청 컨텍스트를 찾을 수 없습니다. (RequestAttributes is null)");
-            throw new IllegalStateException("해당 AOP는 HTTP 요청 내부에서만 실행 가능합니다.");
-        }
-
-        // 요청 정보 가져오기
-        HttpServletRequest request = attributes.getRequest();
+        HttpServletRequest request = getRequest();
 
         //url 정보
         String url = request.getRequestURI();
@@ -66,5 +54,16 @@ public class LoggingAspect {
         log.info("<<< 요청 ID= {} | API Response:[Body: {}]",requestId, responseBody);
 
         return result;
+    }
+
+    private HttpServletRequest getRequest() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if(attributes == null){
+            log.error("HTTP 요청 컨텍스트를 찾을 수 없습니다. (RequestAttributes is null)");
+            throw new IllegalStateException("해당 AOP는 HTTP 요청 내부에서만 실행 가능합니다.");
+        }
+
+        return attributes.getRequest();
     }
 }
